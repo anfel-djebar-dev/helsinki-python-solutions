d25efcc066b1163634cd4a31d51f4bfb32f7431b
Anfel Djebar <anfeldjebar@gmail.com>
2026-07-28 18:41:49 +0100

    feat: add part X exercises

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,8 @@
-# Write your solution here
+number_of_items= int(input("How many items: "))
+i=1
+list_of_item = []
+while i <= number_of_items: 
+    item = int(input(f"Item {i}: "))
+    list_of_item.append(item)
+    i+=1
+print(list_of_item)
