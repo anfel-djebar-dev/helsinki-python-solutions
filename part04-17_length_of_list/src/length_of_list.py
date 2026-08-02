5a4f7d2bddae192aa4fe43fb750468182b98859c
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-02 21:02:23 +0100

    feat: add part X exercises

--- part04-17_length_of_list/src/length_of_list.py
+++ part04-17_length_of_list/src/length_of_list.py
@@ -1,5 +1,13 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+# --------- The Creation Of The Function : ---------
+def length( my_list):
+    len(my_list)
+    return len(my_list)
+
+# Testing the function: 
+#my_list = [3, 6, -4]
+#result = length(my_list)
+#print(result)
+
 if __name__ == "__main__":
     my_list = [3, 6, -4]
     result = length(my_list)
