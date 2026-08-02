27773e830a7ebd17de1c6beb6b1c6eca84be4297
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-02 20:55:43 +0100

    feat: add part X exercises

--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,5 +1,11 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+# ---------- The creation Of The Function : ----------
+def range_of_list(my_list):
+    # range the list from the smallest to the greatest with : sorted()
+    new_list = sorted(my_list)
+    # calcule the difference between the greatest and the smallest number (item)
+    result = new_list[-1] - new_list[0] 
+    return result 
+
 if __name__ == "__main__":
     my_list = [3, 6, -4]
     result = range_of_list(my_list)
