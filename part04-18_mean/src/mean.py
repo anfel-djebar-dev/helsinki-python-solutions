860c220cf88f64a10876858632d4e2574c241032
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-02 21:02:41 +0100

    feat: add part X exercises

--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,5 +1,8 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+# ---------- The Creation Of The Funciton: ----------
+def mean(my_list):
+     the_mean = sum(my_list) / len(my_list)
+     return the_mean
+    
 if __name__ == "__main__":
     my_list = [3, 6, -4]
     result = mean(my_list)
