5e7b62935955c4654cdb57d738d301a2cf29d0ab
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-04 13:32:46 +0100

    feat: add part X exercises

--- part04-24_palindromes/src/palindromes.py
+++ part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,35 @@
-# Write your solution here
-# Note, that at this time the main program should not be written inside
-# if __name__ == "__main__":
-# block!
+# ------------ Creation of the funciton : -------------
+def palindromes(word):
+
+    #initialization of i :
+    i = len(word)
+    
+    #the loop for :
+    for n in range(len(word)):
+
+        i-=1
+        if word[n] != word[i]:
+            return False
+        if n >= i:              
+            return True    
+    
+#--------the loop while: -------
+while True:
+
+    # 1/enter the word:
+    word = input("Please type in a palindrome: ")
+
+    # 2/calling the function:
+    if palindromes(word):
+        print(f"{word} is a palindrome!")
+        break
+    else:
+        print("that wasn't a palindrome")
+
+if __name__ == "__main__":
+    palindromes(word)
+    
+                   
+               
+
+
