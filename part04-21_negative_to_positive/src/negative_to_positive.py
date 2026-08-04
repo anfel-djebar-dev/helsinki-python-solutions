579215c9ad209c8c9c6ed4ff32ed9e3644e75711
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-04 10:05:16 +0100

    feat: add part X exercises

--- part04-21_negative_to_positive/src/negative_to_positive.py
+++ part04-21_negative_to_positive/src/negative_to_positive.py
@@ -1,1 +1,10 @@
-# Write your solution here
+ # --------- Enter the number N : --------
+N = int(input("Please type in a positive integar : "))
+
+
+# ----------The loop For : ------------
+for n in range(-N , N, 1) :
+    if n == 0: 
+        continue
+    print(n)
+print(N)
