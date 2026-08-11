fabb1ecaeba8cac21d0ee17e3191804011c1e0ed
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-11 23:27:39 +0100

    feat: add part X exercises

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def everything_reversed(my_list: list) :
+    new_list = []
+    for item in my_list:
+        new_list.append(item[::-1])
+    
+    return new_list[::-1]
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
