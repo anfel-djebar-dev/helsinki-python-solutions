1ffc630c43f3f21d6556a197854037ebb0c832f3
Anfel Djebar <anfeldjebar@gmail.com>
2026-08-14 21:47:56 +0100

    feat: add part X exercises

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,13 @@
-# Write your solution here
+def no_vowels(my_string: str) :
+    vowels = "aeiou"
+    new_string = ""
+    
+    for char in my_string:
+        if char not in vowels:
+            new_string += char
+            
+    return new_string
+
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
