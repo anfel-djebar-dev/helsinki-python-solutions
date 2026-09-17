4d746dacc1d7de54323300d749532d88cddcde19
Anfel Djebar <anfeldjebar@gmail.com>
2026-09-17 17:04:59 +0100

    feat: solve new exercise

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,14 @@
-# Write your solution here
+# function to count how many elements within the matrix match the argument value :
+def count_matching_elements(matrix: list , element: int):
+    count = 0
+    for row in matrix :
+        for column in row :
+            if column == element :
+                count +=1 
+    return count
+
+# the main function :
+if __name__ == "__main__" :
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
+                
