fff49eb0ec212f5c53bea3893608a603381aa7fa
Anfel Djebar <anfeldjebar@gmail.com>
2026-09-18 21:12:08 +0100

    feat: solve new exercise

--- part05-04_sudoku_row/src/sudoku_row.py
+++ part05-04_sudoku_row/src/sudoku_row.py
@@ -1,1 +1,10 @@
-# Write your solution here
+# -------- The function : ----------
+def row_correct(sudoku: list , row_no: int) -> bool :
+    row = sudoku[row_no] # for go directly to the row
+     
+    members_only = [square for square in row if square != 0 ]
+    # this line will make a list with all the element of the row != 0 
+
+    if len(members_only) != len(set(members_only)):
+        return False
+    return True
