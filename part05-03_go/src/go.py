e58b168417b4b8633b529e43606e257262f5a3d4
Anfel Djebar <anfeldjebar@gmail.com>
2026-09-18 09:14:06 +0100

    feat: solve new exercise

--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,16 @@
-# Write your solution here
+# -----------------The function : -------------
+def who_won(gameboard: list):
+    count_player1 = 0
+    count_player2 = 0
+    for row in gameboard:
+        for square in row :
+            if square == 1 :
+                count_player1 +=1 
+            elif square == 2 :
+                count_player2 += 1
+    if count_player1 > count_player2 :
+        return 1
+    elif count_player1 < count_player2:
+        return 2
+    else :
+        return 0
